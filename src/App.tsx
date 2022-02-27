444e02697a13079f32a8417e3bbfa0360bd76863
Marcos Andre <marcosdev.me@gmail.com>
2022-02-26 21:36:54 -0300

    Add base of project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { GlobalStyles } from "./styles/GlobalStyles";
+
+const App = () => {
+  return (
+    <>
+      <h1>React Tesla</h1>
+
+      <GlobalStyles />
+    </>
+  );
+};
+
+export default App;
