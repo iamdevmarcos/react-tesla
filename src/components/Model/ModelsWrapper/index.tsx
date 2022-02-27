07fd5fb9af43bc76d0fa8fe5cc5e6df9f0ed08ed
Marcos Andre <marcosdev.me@gmail.com>
2022-02-27 09:09:38 -0300

    feat: add initial context

--- src/components/Model/ModelsWrapper/index.tsx
+++ src/components/Model/ModelsWrapper/index.tsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useCallback, useRef, useState } from "react";
+import { CarModel } from "../ModelsContext";
 
 import { Container } from "./styles";
 
 const ModelsWrapper: React.FC = ({ children }) => {
-  return <Container>{children}</Container>;
+  const wrapperRef = useRef<HTMLDivElement>(null);
+
+  const [registeredModels, setRegisteredModels] = useState<CarModel[]>([]);
+
+  const registerModel = useCallback((model: CarModel) => {
+    setRegisteredModels((state) => [...state, model]);
+  }, []);
+
+  const unregisterModel = useCallback((modelName: string) => {
+    setRegisteredModels((state) =>
+      state.filter((model) => model.modelName === modelName)
+    );
+  }, []);
+
+  const getModelByName = useCallback((modelName: string) => {
+    return registeredModels.find((item) => item.modelName === modelName);
+  }, []);
+
+  return <Container ref={wrapperRef}>{children}</Container>;
 };
 
 export default ModelsWrapper;
